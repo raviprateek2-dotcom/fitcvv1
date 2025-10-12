04dba901ed4ec9efffdc10f99dc51d8b31057311
Ravi Prateek <ravi.prateek2@gmail.com>
2025-10-12 08:37:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8605399035-5d3a3",
+  "appId": "1:170986632877:web:9187f691f674d67d5c3824",
+  "apiKey": "AIzaSyBAG-JJXayLs9J6B7E8XNoal-FLbZwpO1w",
+  "authDomain": "studio-8605399035-5d3a3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "170986632877"
+};
