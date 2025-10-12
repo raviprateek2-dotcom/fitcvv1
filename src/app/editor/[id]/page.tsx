b70faced6d3734389109ff9ec2a34c9e4d2b98ed
Ravi Prateek <ravi.prateek2@gmail.com>
2025-10-12 09:01:34 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/editor/[id]/page.tsx
+++ src/app/editor/[id]/page.tsx
@@ -4,11 +4,10 @@
 import { ResumeEditor } from '@/components/editor/ResumeEditor';
 
 export default function EditorPage({ params }: { params: { id: string } }) {
-  const { id } = params;
   return (
     <div className="flex flex-col h-screen bg-secondary">
       <div className="flex-grow overflow-hidden">
-        <ResumeEditor resumeId={id} />
+        <ResumeEditor resumeId={params.id} />
       </div>
     </div>
   );
