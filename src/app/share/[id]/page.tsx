6458cffff08c1da846c678dbe226157a097c53c3
Ravi Prateek <ravi.prateek2@gmail.com>
2025-10-13 11:50:43 +0000

    continue

--- src/app/share/[id]/page.tsx
+++ src/app/share/[id]/page.tsx
@@ -3,9 +3,8 @@
 
 import { useCollection, useDoc, useFirestore, useMemoFirebase } from '@/firebase';
 import { addDoc, collection, serverTimestamp, doc } from 'firebase/firestore';
-import { notFound } from 'next/navigation';
 import { ResumePreview } from '@/components/editor/ResumePreview';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
