8de17803e57e58bac1cfc06770ce82ac9133d484
Ravi Prateek <ravi.prateek2@gmail.com>
2025-10-14 02:27:27 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/lib/pdf-parse/index.js
+++ src/lib/pdf-parse/index.js
@@ -12,7 +12,7 @@ async function PDF(dataBuffer, options) {
     } = options || {};
 
     //PDFJS should be dynamically required
-    const PDFJS = require('./pdf.js/v2.0.550/build/pdf.js');
+    const PDFJS = require(path.join(__dirname, 'pdf.js/v2.0.550/build/pdf.js'));
 
     //make sure data is a buffer
     if (!isBuffer(dataBuffer)) {
