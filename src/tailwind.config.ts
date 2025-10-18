cd2b3a818da5b39411e6c40db0fb0547429002f7
Ravi Prateek <ravi.prateek2@gmail.com>
2025-10-18 14:41:53 +0000

    continue

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -87,17 +87,16 @@ const config = {
           '0%': { transform: 'translateX(-100%)' },
           '100%': { transform: 'translateX(100%)' },
         },
-        gradient: {
-          '0%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-          '100%': { backgroundPosition: '0% 50%' },
+        'shimmer-text': {
+          '0%, 100%': { backgroundPosition: '200% center' },
+          '50%': { backgroundPosition: '-200% center' },
         },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         shimmer: 'shimmer 2s infinite linear',
-        gradient: 'gradient 5s ease infinite',
+        'shimmer-text': 'shimmer-text 5s linear infinite',
       },
     },
   },
